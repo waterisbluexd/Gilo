e6714e3933ee898200bb11601e0807e269b9d8e3
waterisblue <98446139+waterisbluexd@users.noreply.github.com>
2025-11-11 17:58:28 +0530

    Added Tower
    Twoer Wall1 wall2

--- SCRIPTS/UI/Builders/element_panel.gd
+++ SCRIPTS/UI/Builders/element_panel.gd
@@ -4,6 +4,7 @@ extends Panel
 @onready var hovels_button: Button = $House_Select/MarginContainer/GridContainer/Hovels_Panel/Hovels_Button
 @onready var type_wall_1_button: Button = $Castle_Select/MarginContainer/GridContainer/Panel/Type_Wall_1_button
 @onready var type_wall_2_button: Button = $Castle_Select/MarginContainer/GridContainer/Panel2/Type_Wall_2_button
+@onready var type_tower_1_button: Button = $Castle_Select/MarginContainer/GridContainer/Panel3/Type_Tower_1_button
 
 var building_placer: BuildingPlacer
 
@@ -18,6 +19,7 @@ func _ready():
 	hovels_button.pressed.connect(_on_hovels_pressed)
 	type_wall_1_button.pressed.connect(_on_type_wall_1_pressed)
 	type_wall_2_button.pressed.connect(_on_type_wall_2_button_pressed)
+	type_tower_1_button.pressed.connect(_on_type_tower_1_pressed)
 
 
 func _on_tent_pressed():
@@ -36,3 +38,7 @@ func _on_type_wall_1_pressed():
 func _on_type_wall_2_button_pressed():
 	if building_placer:
 		building_placer.select_building(12)
+
+func _on_type_tower_1_pressed():
+	if building_placer:
+		building_placer.select_building(20)
