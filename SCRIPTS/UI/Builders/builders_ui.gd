e65cdd6326e7f973b1f6e308b1857bc22544ddd3
waterisblue <98446139+waterisbluexd@users.noreply.github.com>
2025-11-15 16:50:01 +0530

    Building Sleectpor

--- SCRIPTS/UI/Builders/builders_ui.gd
+++ SCRIPTS/UI/Builders/builders_ui.gd
@@ -1,40 +1,54 @@
 extends Control
-
+#######################
 # Openers
-@onready var castle_panel_button: Button = $HBoxContainer/Castle_Panel/Castle_Panel_Button
-@onready var crafts_panel_button: Button = $HBoxContainer/Crafts_Panel/Crafts_Panel_Button
-@onready var house_panel_button: Button = $HBoxContainer/House_Panel/House_Panel_Button
-
+@onready var castle_clip_button: Button = $Control/Clips/Castle/Castle_Clip_button
+@onready var crafts_clip_button: Button = $Control/Clips/Crafts/Crafts_Clip_Button
+@onready var armors_clip_button: Button = $Control/Clips/Armors/Armors_Clip_Button
+@onready var houses_clip_button: Button = $Control/Clips/Houses/Houses_Clip_Button
+@onready var farm_clip_button: Button = $Control/Clips/Farm/Farm_Clip_Button
+@onready var bakery_clip_button: Button = $Control/Clips/Bakery/Bakery_Clip_Button
 # Selectors
-@onready var castle_select: Control = $Element_Panel/Castle_Select
-@onready var house_select: Control = $Element_Panel/House_Select
-@onready var craft_select: Control = $Element_Panel/Craft_Select
-
-
-# Array to store all selectors
+@onready var castle_item_selector: Control = $Control/Book/LeftPage/MarginContainer/Castle_Item_Selector
+@onready var crafts_item_selector: Control = $Control/Book/LeftPage/MarginContainer/Crafts_Item_Selector
+@onready var armors_item_selector: Control = $Control/Book/LeftPage/MarginContainer/Armors_Item_Selector
+@onready var houses_item_selector: Control = $Control/Book/LeftPage/MarginContainer/Houses_Item_Selector
+@onready var farm_item_selector: Control = $Control/Book/LeftPage/MarginContainer/Farm_Item_Selector
+@onready var bakery_item_selector: Control = $Control/Book/LeftPage/MarginContainer/Bakery_Item_Selector
+########################
 var selectors: Array[Control] = []
 
 func _ready() -> void:
-	selectors = [castle_select, house_select, craft_select]
+	selectors = [castle_item_selector, crafts_item_selector, armors_item_selector, houses_item_selector, farm_item_selector, bakery_item_selector]
 	
-	hide_all_selectors()
-
-	castle_select.visible = true
-
-func hide_all_selectors() -> void:
-	for selector in selectors:
-		if selector:  # Check if selector exists
-			selector.visible = false
+	# Connect button signals
+	castle_clip_button.pressed.connect(_on_castle_button_pressed)
+	crafts_clip_button.pressed.connect(_on_crafts_button_pressed)
+	armors_clip_button.pressed.connect(_on_armors_button_pressed)
+	houses_clip_button.pressed.connect(_on_houses_button_pressed)
+	farm_clip_button.pressed.connect(_on_farm_button_pressed)
+	bakery_clip_button.pressed.connect(_on_bakery_button_pressed)
+	
+	# Show only castle selector on start
+	show_selector(castle_item_selector)
 
 func show_selector(selector_to_show: Control) -> void:
-	hide_all_selectors()
-	selector_to_show.visible = true
+	for selector in selectors:
+		selector.visible = (selector == selector_to_show)
 
-func _on_castle_panel_button_pressed() -> void:
-	show_selector(castle_select)
+func _on_castle_button_pressed() -> void:
+	show_selector(castle_item_selector)
 
-func _on_house_panel_button_pressed() -> void:
-	show_selector(house_select)
+func _on_crafts_button_pressed() -> void:
+	show_selector(crafts_item_selector)
 
-func _on_crafts_panel_button_pressed() -> void:
-	show_selector(craft_select)
+func _on_armors_button_pressed() -> void:
+	show_selector(armors_item_selector)
+
+func _on_houses_button_pressed() -> void:
+	show_selector(houses_item_selector)
+
+func _on_farm_button_pressed() -> void:
+	show_selector(farm_item_selector)
+
+func _on_bakery_button_pressed() -> void:
+	show_selector(bakery_item_selector)
--- SCRIPTS/UI/Builders/element_panel.gd
+++ SCRIPTS/UI/Builders/element_panel.gd
@@ -1,12 +1,23 @@
-extends Panel
+extends HBoxContainer
 
-@onready var tent_button: Button = $House_Select/MarginContainer/GridContainer/Tent_Panel/Tent_Button
-@onready var hovels_button: Button = $House_Select/MarginContainer/GridContainer/Hovels_Panel/Hovels_Button
-@onready var type_wall_1_button: Button = $Castle_Select/MarginContainer/GridContainer/Panel/Type_Wall_1_button
-@onready var type_wall_2_button: Button = $Castle_Select/MarginContainer/GridContainer/Panel2/Type_Wall_2_button
-@onready var type_tower_1_button: Button = $Castle_Select/MarginContainer/GridContainer/Panel3/Type_Tower_1_button
-@onready var type_tower_2_button: Button = $Castle_Select/MarginContainer/GridContainer/Panel4/Type_Tower_2_button
-@onready var type_tower_3_button: Button = $Castle_Select/MarginContainer/GridContainer/Panel5/Type_Tower_3_button
+# Castle buttons
+@onready var wall_1_button: Button = $LeftPage/MarginContainer/Castle_Item_Selector/GridContainer/Wall_1/Button
+@onready var wall_2_button: Button = $LeftPage/MarginContainer/Castle_Item_Selector/GridContainer/Wall_2/Button
+@onready var tower_1_button: Button = $LeftPage/MarginContainer/Castle_Item_Selector/GridContainer/Tower_1/Button
+@onready var tower_2_button: Button = $LeftPage/MarginContainer/Castle_Item_Selector/GridContainer/Tower_2/Button
+@onready var tower_3_button: Button = $LeftPage/MarginContainer/Castle_Item_Selector/GridContainer/Tower_3/Button
+
+# Crafts buttons
+@onready var stock_pile_button: Button = $LeftPage/MarginContainer/Crafts_Item_Selector/GridContainer/StockPile/Button
+@onready var wood_cutter_button: Button = $LeftPage/MarginContainer/Crafts_Item_Selector/GridContainer/WoodCutter/Button
+@onready var stone_cutter_button: Button = $LeftPage/MarginContainer/Crafts_Item_Selector/GridContainer/StoneCutter/Button
+@onready var bulk_button: Button = $LeftPage/MarginContainer/Crafts_Item_Selector/GridContainer/Bulk/Button
+@onready var iron_mine_button: Button = $LeftPage/MarginContainer/Crafts_Item_Selector/GridContainer/IronMine/Button
+
+# Houses buttons
+@onready var tent_button: Button = $LeftPage/MarginContainer/Houses_Item_Selector/GridContainer/Tent/Button
+@onready var houses_button: Button = $LeftPage/MarginContainer/Houses_Item_Selector/GridContainer/Houses/Button
+@onready var well_button: Button = $LeftPage/MarginContainer/Houses_Item_Selector/GridContainer/well/Button
 
 var building_placer: BuildingPlacer
 
@@ -16,41 +27,119 @@ func _ready():
 	if not building_placer:
 		return
 	
-	# Connect buttons
-	tent_button.pressed.connect(_on_tent_pressed)
-	hovels_button.pressed.connect(_on_hovels_pressed)
-	type_wall_1_button.pressed.connect(_on_type_wall_1_pressed)
-	type_wall_2_button.pressed.connect(_on_type_wall_2_button_pressed)
-	type_tower_1_button.pressed.connect(_on_type_tower_1_pressed)
-	type_tower_2_button.pressed.connect(_on_type_tower_2_pressed)
-	type_tower_3_button.pressed.connect(_on_type_tower_3_pressed)
+	# Connect Castle buttons
+	if wall_1_button:
+		wall_1_button.pressed.connect(_on_wall_1_pressed)
+	else:
+		print("wall_1_button value not set")
+	
+	if wall_2_button:
+		wall_2_button.pressed.connect(_on_wall_2_pressed)
+	else:
+		print("wall_2_button value not set")
+	
+	if tower_1_button:
+		tower_1_button.pressed.connect(_on_tower_1_pressed)
+	else:
+		print("tower_1_button value not set")
+	
+	if tower_2_button:
+		tower_2_button.pressed.connect(_on_tower_2_pressed)
+	else:
+		print("tower_2_button value not set")
+	
+	if tower_3_button:
+		tower_3_button.pressed.connect(_on_tower_3_pressed)
+	else:
+		print("tower_3_button value not set")
+	
+	# Connect Crafts buttons
+	if stock_pile_button:
+		stock_pile_button.pressed.connect(_on_stock_pile_pressed)
+	else:
+		print("stock_pile_button value not set")
+	
+	if wood_cutter_button:
+		wood_cutter_button.pressed.connect(_on_wood_cutter_pressed)
+	else:
+		print("wood_cutter_button value not set")
+	
+	if stone_cutter_button:
+		stone_cutter_button.pressed.connect(_on_stone_cutter_pressed)
+	else:
+		print("stone_cutter_button value not set")
+	
+	if bulk_button:
+		bulk_button.pressed.connect(_on_bulk_pressed)
+	else:
+		print("bulk_button value not set")
+	
+	if iron_mine_button:
+		iron_mine_button.pressed.connect(_on_iron_mine_pressed)
+	else:
+		print("iron_mine_button value not set")
+	
+	# Connect Houses buttons
+	if tent_button:
+		tent_button.pressed.connect(_on_tent_pressed)
+	else:
+		print("tent_button value not set")
+	
+	if houses_button:
+		houses_button.pressed.connect(_on_houses_pressed)
+	else:
+		print("houses_button value not set")
+	
+	if well_button:
+		well_button.pressed.connect(_on_well_pressed)
+	else:
+		print("well_button value not set")
 
+# Castle callbacks
+func _on_wall_1_pressed():
+	if building_placer:
+		building_placer.select_building(10)
 
+func _on_wall_2_pressed():
+	if building_placer:
+		building_placer.select_building(11)
+
+func _on_tower_1_pressed():
+	if building_placer:
+		building_placer.select_building(22)
+
+func _on_tower_2_pressed():
+	if building_placer:
+		building_placer.select_building(19)
+
+func _on_tower_3_pressed():
+	if building_placer:
+		building_placer.select_building(20)
+
+# Crafts callbacks
+func _on_stock_pile_pressed():
+	print("stock_pile value not set")
+
+func _on_wood_cutter_pressed():
+	print("wood_cutter value not set")
+
+func _on_stone_cutter_pressed():
+	print("stone_cutter value not set")
+
+func _on_bulk_pressed():
+	print("bulk value not set")
+
+func _on_iron_mine_pressed():
+	print("iron_mine value not set")
+
+# Houses callbacks
 func _on_tent_pressed():
 	if building_placer:
 		building_placer.select_building_category(7, 9)
 
-
-func _on_hovels_pressed():
+func _on_houses_pressed():
 	if building_placer:
 		building_placer.select_building_category(12, 17)
 
-func _on_type_wall_1_pressed():
-	if building_placer:
-		building_placer.select_building(10)
-
-func _on_type_wall_2_button_pressed():
-	if building_placer:
-		building_placer.select_building(11)
-
-func _on_type_tower_1_pressed():
-	if building_placer:
-		building_placer.select_building(19)
-
-func _on_type_tower_2_pressed():
-	if building_placer:
-		building_placer.select_building_category(20, 21)
-
-func _on_type_tower_3_pressed():
-	if building_placer:
-		building_placer.select_building(22)
+func _on_well_pressed():
+	print("well value not set")
